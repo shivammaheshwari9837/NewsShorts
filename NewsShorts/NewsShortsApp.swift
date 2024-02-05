f6847361e6a4f9337aee733629010d5815a73799
Shivam Maheshwari <shmaheshwari@expediagroup.com>
2024-02-05 21:59:43 +0530

    Initial Commit

---
+++ NewsShorts/NewsShortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsShortsApp.swift
+//  NewsShorts
+//
+//  Created by Shivam Maheshwari on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsShortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
